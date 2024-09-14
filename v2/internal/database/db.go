ad3fa07a386138ce98e339afb57fe597bbedfbae
SkylarHoughtonGithub <SkylarHoughton@Github.com>
2024-09-13 23:06:46 -0500

    if table not exist

--- v2/internal/database/db.go
+++ v2/internal/database/db.go
@@ -26,7 +26,7 @@ func NewDB(dataSourceName string) (*DB, error) {
 
 func CreateTable(db *DB) error {
 	createTableSQL := `
-    CREATE TABLE books (
+    CREATE TABLE IF NOT EXISTS books (
         id SERIAL PRIMARY KEY,
         title VARCHAR(255) NOT NULL,
         author VARCHAR(255) NOT NULL
