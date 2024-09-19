6ae62a4f49c2edc3893ae694a1f56221a596ad78
SkylarHoughtonGithub <SkylarHoughton@Github.com>
2024-09-18 20:53:39 -0500

    new build workflow

--- build.sh
+++ build.sh
@@ -1,1 +1,12 @@
-go build -o skylab-book-chameleon ./cmd/server
+#!/bin/bash
+set -e
+
+# Use the first argument as APP_NAME, or fall back to "myapp" if not provided
+NAME=$1
+
+echo "Building $NAME..."
+
+# Build binary
+go build -o $NAME ./cmd/server
+
+echo "$NAME built successfully!"
--- init.sh
+++ init.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+set -e
+
+# Use the first argument as APP_NAME, or fall back to "myapp" if not provided
+NAME=$1
+
+echo "init module $NAME..."
+
+# Download dependencies
+go mod init $NAME
 go mod tidy
