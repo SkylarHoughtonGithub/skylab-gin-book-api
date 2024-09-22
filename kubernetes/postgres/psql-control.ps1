179ee32d00c2544a3ed46c41dd1107a12cf2d435
SkylarHoughtonGithub <SkylarHoughton@Github.com>
2024-09-21 19:21:56 -0500

    psql-control mod

--- kubernetes/postgres/psql-control.ps1
+++ kubernetes/postgres/psql-control.ps1
@@ -1,13 +1,18 @@
 # Define kubectl function and alias
-function kubectl { minikube kubectl -- $args }
-New-Alias k kubectl
 
 # Define the paths to your YAML files
 $pvcPath = "pvc.yml"
 $secretPath = "secret.yml"
 $deploymentPath = "deployment.yml"
 $servicePath = "service.yml"
+function kubectl { minikube kubectl -- $args }
+New-Alias k kubectl
 
+function init {
+    Write-Host "Preparing env..."
+    minikube start
+    Write-Host "env started."
+}
 function apply {
     Write-Host "Creating PostgreSQL resources..."
     k apply -f $pvcPath
@@ -44,6 +49,8 @@ function refresh {
 function Show-Usage {
     Write-Host "Usage: .\psql-control.ps1 <command>"
     Write-Host "Commands:"
+    Write-Host "  init    - Prepare dev psql environment."
+    Write-Host "  create  - Create PostgreSQL resources"
     Write-Host "  create  - Create PostgreSQL resources"
     Write-Host "  destroy - Destroy PostgreSQL resources"
     Write-Host "  refresh - Destroy and Create PostgreSQL resources"
@@ -56,6 +63,9 @@ if ($args.Count -eq 0) {
 }
 
 switch ($args[0]) {
+    "init" {
+        init
+    }
     "create" {
         apply
     }
