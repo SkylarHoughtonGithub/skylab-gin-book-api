ca6b7b59cc0db33761629f11af51e8184671d888
SkylarHoughtonGithub <SkylarHoughton@Github.com>
2024-09-26 18:30:19 -0500

    better main unit tests

--- cmd/server/main_test.go
+++ cmd/server/main_test.go
@@ -1,18 +1,56 @@
-// cmd/server/main.go
-
 package main
 
-import "testing"
+import (
+	"log"
+	"skylab-book-chameleon/internal/config"
+	"testing"
 
-func Test_main(t *testing.T) {
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSetupEnvironment(t *testing.T) {
 	tests := []struct {
-		name string
+		name     string
+		cfg      *config.Config
+		wantMode string
 	}{
-		// TODO: Add test cases.
+		{
+			name:     "Debug_mode",
+			cfg:      &config.Config{Server: config.ServerConfig{Debug: true}},
+			wantMode: gin.DebugMode,
+		},
+		{
+			name:     "Release_mode",
+			cfg:      &config.Config{Server: config.ServerConfig{Debug: false}},
+			wantMode: gin.ReleaseMode,
+		},
 	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			main()
+			err := setupEnvironment(tt.cfg)
+			assert.NoError(t, err)
+			assert.Equal(t, tt.wantMode, gin.Mode())
+		})
+	}
+}
+
+func TestSetLogLevel(t *testing.T) {
+	tests := []struct {
+		name  string
+		level string
+		want  int
+	}{
+		{"Debug_level", "debug", log.Ldate | log.Ltime | log.Lshortfile},
+		{"Info_level", "info", log.Ldate | log.Ltime},
+		{"Default_level", "error", 0},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			setLogLevel(tt.level)
+			assert.Equal(t, tt.want, log.Flags())
 		})
 	}
 }
